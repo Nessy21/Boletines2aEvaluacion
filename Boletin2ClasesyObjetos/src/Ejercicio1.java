b1f6d3e1e735fd72dd37071eb05b94d697ec6fe8
alen <alen@DESKTOP-3V7KHMG>
2020-01-14 12:27:19 +0100

    .asd

---
+++ Boletin2ClasesyObjetos/src/Ejercicio1.java
@@ -0,0 +1,13 @@
+
+import java.util.Date; 
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		//
+	}
+	class Persona {
+		public String nombre;
+		public String apellidos;
+		public int estatura;
+		public Date fechaNac;
+	}
+}
