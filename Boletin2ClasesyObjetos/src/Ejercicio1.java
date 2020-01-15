701649b5c8abf340f87fae403d51e1013dbd72ab
alen <alen@DESKTOP-3V7KHMG>
2020-01-15 14:23:46 +0100

    .

--- Boletin2ClasesyObjetos/src/Ejercicio1.java
+++ Boletin2ClasesyObjetos/src/Ejercicio1.java
@@ -1,13 +1,51 @@
+import java.time.LocalDate;
 
-import java.util.Date; 
 public class Ejercicio1 {
-	public static void main(String[] args) {
-		//
+
+
+		public static void main(String[] args) {
+		
+			Persona p1 = new Persona();
+			p1.nombre = "Jose";
+			p1.apellidos = "Rodríguez Mata";
+			p1.fechaNac = LocalDate.of(1965, 01, 15);
+			p1.estatura=165;
+			
+			p1.mostrarDatos();
+			
+			
+			Persona p2 = new Persona();
+			p2.nombre = "María";
+			p2.apellidos = "Sánchez Gómez";
+			p2.fechaNac = LocalDate.of(1980, 12, 26);
+			p2.estatura=170;
+			
+			p2.mostrarDatos();
+		
+			Persona p3 = new Persona();
+			p3.nombre = "Alejandro";
+			p3.apellidos = "Gómez Gómez";
+			p3.fechaNac = LocalDate.of(2001, 11, 01);
+			p3.estatura=190;
+			
+			p3.mostrarDatos();
+			
+			Persona p4 = new Persona();
+			p4.nombre = "Jose Manuel";
+			p4.apellidos = "Pérez Pons";
+			p4.fechaNac = LocalDate.of(2010, 05, 01);
+			p4.estatura=127;
+			
+			p4.mostrarDatos();
+			
+			Persona p5 = new Persona ("Lorena", "Remeseiro Neira", LocalDate.of(2000, 06, 12));
+			p5.estatura=160;
+			p5.mostrarDatos();
+			
+			Persona p6 = new Persona ("Patricia", "Seoane Álvarez", LocalDate.of(1976, 8, 23));
+			p6.estatura=175;
+			p6.mostrarDatos();
+			
 	}
-	class Persona {
-		public String nombre;
-		public String apellidos;
-		public int estatura;
-		public Date fechaNac;
-	}
+
 }
---
+++ Boletin2ClasesyObjetos/src/Persona.java
@@ -0,0 +1,47 @@
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+	public class Persona {
+	
+			// campos
+			public String nombre;
+			public String apellidos;
+			public int estatura;
+			public LocalDate fechaNac;
+			public long edad;
+		
+
+
+
+		//métodos
+
+	public void mostrarDatos() {
+			System.out.println(nombre);
+			System.out.println(apellidos);
+			System.out.println(estatura + " cm");
+			edad = obtenerEdad();
+			System.out.println(fechaNac + "\n");
+			
+	
+	}//mostrarDatos
+	
+	public long obtenerEdad() {
+	
+		Long edad = ChronoUnit.YEARS.between(fechaNac, LocalDate.now());
+		System.out.println(edad + " años");
+		return edad;
+	}//obtenerEdad
+
+
+//constructores
+	
+public Persona (String nom, String apell, LocalDate fechNac ){
+	nombre=nom;
+	apellidos=apell;
+	fechaNac= fechNac;
+}
+public Persona () {}//vacio que es el por defecto, en cuanto haces un constructor el por defecto desaparece.
+
+}//persona
+
