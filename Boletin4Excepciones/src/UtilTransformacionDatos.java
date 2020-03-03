ff1fabbc4b25db70a6788fa3035e6c3d832f4ab6
alen <alen@DESKTOP-3V7KHMG>
2020-03-03 12:58:15 +0100

    .

---
+++ Boletin4Excepciones/src/UtilTransformacionDatos.java
@@ -0,0 +1,21 @@
+
+public class UtilTransformacionDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static boolean esValorNumerico () {
+		boolean esValorNumerico = true;
+		String cad="17";
+		int convNum= Integer.parseInt(cad);
+		
+		
+		
+		
+		return esValorNumerico;
+	}
+	
+
+}
