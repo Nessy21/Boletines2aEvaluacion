9926ed261135891a14500e88bca0f0d7acfaae48
alen <alen@DESKTOP-3V7KHMG>
2020-03-04 14:13:32 +0100

    .

--- Boletin4Excepciones/src/UtilTransformacionDatos.java
+++ Boletin4Excepciones/src/UtilTransformacionDatos.java
@@ -2,20 +2,50 @@
 public class UtilTransformacionDatos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		String cad="45";//en caso de poner algo que no sea numerico peta
+		String cadDouble="45.2";
+		 
+		if (esValorNumerico(cad)) {
+			System.out.println("Es valor numérico \n");
+		}
+		else {
+			
+			System.out.println("No es valor numérico\n");
+		}
+		
+		
+		double llamada=obtenerValorDouble(cadDouble);
+	}//main
 	
-	public static boolean esValorNumerico () {
+	public static boolean esValorNumerico (String cadena) {
 		boolean esValorNumerico = true;
-		String cad="17";
-		int convNum= Integer.parseInt(cad);
 		
 		
+		try {
+			int convNum= Integer.parseInt(cadena);
+		}
 		
+		catch (NumberFormatException ex) {
+			ex.printStackTrace();//para seguir ejecutando
+			esValorNumerico=false;
+		}
 		
 		return esValorNumerico;
-	}
+	}//esValorNumerico
 	
+	public static double obtenerValorDouble(String cadena) throws IllegalArgumentException{
+		double numd=0;
+	
+		try {
+			 numd= Double.parseDouble(cadena);
+			 System.out.println("Es double");
+		}
+		catch(NumberFormatException ex) {
+			ex.printStackTrace();
+			System.out.println ("Debes introducir números decimales válidos"); 
+		}
+		
+		return numd;
+}//obtenerValorDouble
 
-}
+}//class
